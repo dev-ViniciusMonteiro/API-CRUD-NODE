30b480dced0a8197156b3f3d2dee1eab3461f616
Vinicius019-dev <vinicius.monteiro.orlandi@hotmail.com>
2021-05-24 13:47:44 -0300

    nova Branche

--- api/rotas/index.js
+++ api/rotas/index.js
@@ -3,5 +3,6 @@ const roteador = require('express').Router()
 roteador.get('/',async (req,res)=>{//listar
     res.send("foi")
 })
-
+//alterando pra ver
+console.log("foi")
 module.exports = roteador
